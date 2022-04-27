23794c4de4d2461573c658089583746aef51ccba
Barak Schmookler <bschmookler1@gmail.com>
2022-04-26 20:13:09 -0400

    Updated setup file.

--- mysetup.sh
+++ mysetup.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+export S3_ACCESS_KEY=eicS3read
+export S3_SECRET_KEY=eicS3read
+
+## For using 'official' setup, uncomment line below and comment all following lines
+#source /opt/detector/setup.sh
+
 export DETECTOR=athena
-## May want to use source directory instead...
-## ..if you use the 'share' version, you will need to make a softlink to ip6
-export DETECTOR_PATH=${ATHENA_PREFIX}/share/athena
+## If you use the 'share' version, you will need to make a softlink to ip6
+#export DETECTOR_PATH=${ATHENA_PREFIX}/share/athena
+## May want to use source directory instead
+export DETECTOR_PATH=${ATHENA_PREFIX}/../athena
+
 export DETECTOR_VERSION=master
 export BEAMLINE_CONFIG=ip6
 export BEAMLINE_CONFIG_VERSION=master
@@ -19,5 +27,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${ATHENA_PREFIX}/lib
 
 ## modify PS1 for this detector version
 export PS1="${PS1:-}"
-export PS1="nightly${PS1_SIGIL}>${PS1#*>}"
+export PS1="local${PS1_SIGIL}>${PS1#*>}"
 unset branch
